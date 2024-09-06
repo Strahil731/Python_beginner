230af24f919ad7a43040cff63f353029f6a2a6c1
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-09-06 12:24:44 +0300

    Create Pet-Shop.py

---
+++ Pet-Shop.py
@@ -0,0 +1,8 @@
+dog_food_package_number = int(input())
+cat_food_package_number = int(input())
+
+dog_food_price = dog_food_package_number * 2.50
+cat_food_price = cat_food_package_number * 4
+all_sum = dog_food_price + cat_food_price
+
+print(f"{all_sum} lv.")
